c9dd7b5dba1bc3637cc8520d831747bedc4f4040
Raffael Meyer <raffael@alyf.de>
2019-05-27 18:09:58 +0200

    new-app

---
+++ rechnungswesen_de/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/rechnungswesen_de"
+# docs_base_url = "https://[org_name].github.io/rechnungswesen_de"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Deutsches Rechnungswesen"
