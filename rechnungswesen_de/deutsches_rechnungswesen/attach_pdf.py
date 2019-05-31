7d0139b6a5ce1c69ea5e43bc273fb666a021d81a
Raffael Meyer <raffael@alyf.de>
2019-06-01 01:00:05 +0200

    fix error and refactor

--- rechnungswesen_de/deutsches_rechnungswesen/attach_pdf.py
+++ rechnungswesen_de/deutsches_rechnungswesen/attach_pdf.py
@@ -1,15 +1,18 @@
 import frappe
 from frappe import _
-from frappe.utils.pdf import get_pdf
-from frappe.utils.file_manager import save_file
-from frappe.core.doctype.file.file import create_new_folder
 
-def sales_invoice(sinv, event):
-    sinv_folder = create_folder(_("Sales Invoice"), "Home")
-    cust_folder = create_folder(sinv.customer, sinv_folder)
-    attach_pdf(sinv, sinv.name, cust_folder)
+def sales_invoice(sinv, event=None):
+    execute(sinv.doctype, sinv.name, sinv.customer)
+
+def execute(doctype, name, party):
+    doctype_folder = create_folder(_(doctype), "Home")
+    party_folder = create_folder(party, doctype_folder)
+    pdf_data = get_pdf_data(doctype, name)
+    save_and_attach(pdf_data, doctype, name, party_folder)
 
 def create_folder(folder, parent):
+    """Make sure the folder exists and return it's name."""
+    from frappe.core.doctype.file.file import create_new_folder
     try:
         create_new_folder(folder, "Home")
     except frappe.DuplicateEntryError:
@@ -17,8 +20,17 @@ def create_folder(folder, parent):
 
     return "/".join([parent, folder])
 
-def attach_pdf(doctype, name, folder):
+def get_pdf_data(doctype, name):
+    """Document -> HTML -> PDF."""
     html = frappe.get_print(doctype, name)
-    file_name = "{}.pdf".format(name.replace(" ", "-").replace("/", "-"))
-    content = get_pdf(html)
-    save_file(file_name, content, doctype, name, folder=folder, is_private=1)
+    return frappe.utils.pdf.get_pdf(html)
+
+def save_and_attach(content, to_doctype, to_name, folder):
+    """
+    Save content to disk and create a File document.
+    
+    File document is linked to another document.
+    """
+    from frappe.utils.file_manager import save_file
+    file_name = "{}.pdf".format(to_name.replace(" ", "-").replace("/", "-"))
+    save_file(file_name, content, to_doctype, to_name, folder=folder, is_private=1)
